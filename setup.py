9c18eb08a59992f35f359ab84173d60fe7e9a5dc
Michael Nsiah-Nimo <michael20129648@gmail.com>
2024-09-04 14:34:42 -0500

    logging and exception

--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@ from typing import List
 
 HYPHEN_E_DOT = '-e .'
 
+
 def get_requirements(file_path: str) -> List[str]:
     '''
     This function will return the list of requirements
     '''
     with open(file_path, 'r') as file_obj:
         requirements = file_obj.read().splitlines()
-        requirements = [req.strip() for req in requirements if req.strip() and req != HYPHEN_E_DOT]
-    
-    return requirements
+        requirements = [
+            req.strip() for req in requirements if req.strip() and req != HYPHEN_E_DOT]
 
+    return requirements
 
 setup(
     name='mlproject',
     version='0.0.1',
     author='Michael',
     author_email='mnsihnimo.gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt'),  # Make sure to filter out `-e .`
+    packages=find_packages(), 
+    install_requires=get_requirements('requirements.txt') # Make sure to filter out `-e .`
 )
-
